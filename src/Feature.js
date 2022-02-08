51cb3b06763d03e4ed6ecb492c78ca92f2b1cf81
unknown <solomon@devsdecide.com>
2022-02-08 17:37:24 -0600

    more formatting

---
+++ src/Feature.js
@@ -0,0 +1,32 @@
+function Feature() {
+    return (
+        <div>
+            <h1>Sell More! Sell Faster!</h1>
+
+            <div className="grid gap-8 lg:grid-cols-2 grid-cols-1">
+
+                <div class="card-2 reveal">
+                    <div class="card-text">
+                        <h3>Unlock Your Business’ True Potential</h3>
+                        <ul>
+                            <li>Increase Profits</li>
+                            <li>Increase Profits</li>
+                            <li>Increase Profits</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card reveal">
+                    <div class="card-text">
+                        <h3>Tech We Use</h3>
+                    </div>
+                </div>
+
+            </div>
+            <div class="feature-cta reveal">
+                <a href="contact-us.html" class="primary-cta">Contact Us</a>
+            </div>
+        </div>
+    );
+}
+
+export default Feature;
