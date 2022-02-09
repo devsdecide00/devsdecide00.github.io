5e1940fd1032838eb7f33ac1ff5daf0d2a90170d
Solomon <solomon@devsdecide.com>
2022-02-08 22:59:21 -0600

    add animation back

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 function Card({headline, copy}) {
     return (
-        <div className="rounded-2xl p-4 md:p-8 lg:p-12 grid gap-4 md:gap-8 lg:gap-12 bg-purple-900/30">
+        <div className="rounded-2xl p-4 md:p-8 lg:p-12 grid gap-4 md:gap-8 lg:gap-12 bg-purple-900/30 reveal">
             <h3 className="font-thin">{headline}</h3>
             <p>{copy}</p>
             <div className="font-bold text-cta-500 self-end hover:text-cta-700 mr-auto hover:underline">
