57eca283244ffb9e419aaa96c721492aaab87f74
Solomon <solomon@devsdecide.com>
2022-02-08 23:19:59 -0600

    fix contact form for mobile

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -6,21 +6,21 @@ function ContactUs({classes}) {
                 <div className="rounded-2xl p-4 md:p-8 lg:p-12 bg-neutral-900/30">
                     <form action="https://api.devsdecide.com/contact-us" method="POST">
                         <div className="grid gap-4 md:gap-8 lg:gap-12">
-                            <div className="flex items-center justify-between">
+                            <div className="grid sm:flex items-center sm:justify-between">
                                 <label>Name</label>
-                                <input name="name" type="text" required className="w-5/6 text-black" aria-label="name"/>
+                                <input name="name" type="text" required className=" sm:w-5/6 text-black" aria-label="name"/>
                             </div>
-                            <div className="flex items-center justify-between">
+                            <div className="grid sm:flex items-center sm:justify-between">
                                 <label>Email</label>
-                                <input name="email" type="text" required className="w-5/6 text-black" aria-label="email"/>
+                                <input name="email" type="text" required className="sm:w-5/6 text-black" aria-label="email"/>
                             </div>
                             <div className="hidden" id="other">
                                 <input name="other" type="text"/>
                                 <label>Other</label>
                             </div>
-                            <div className="flex items-center justify-between">
+                            <div className="grid sm:flex items-center sm:justify-between">
                                 <label>Message</label>
-                                <textarea name="message" required className="w-5/6 text-black" aria-label="message"/>
+                                <textarea name="message" required className="sm:w-5/6 text-black" aria-label="message"/>
                             </div>
                             <div className="no-underline bg-cta-500 rounded-full py-2 md:py-3 px-4 md:px-5 font-bold inline-block hover:bg-cta-700">
                                 <input type="submit" value="Send"/>
