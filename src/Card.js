9486af158d7a63e9d96cf862bea1f672b530c5ca
Solomon <solomon@devsdecide.com>
2022-02-08 23:28:47 -0600

    hide contact us for tech we use card

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,15 @@
-function Card({headline, copy}) {
+function Card({headline, copy, doNotShowContactUs}) {
+    var contactUs = <div className="font-bold text-cta-500 self-end hover:text-cta-700 mr-auto hover:underline">
+        <a className="content-end text-lg md:text-xl lg:text-2xl" href="#contact-us">Contact Us</a>
+    </div>;
+    if(doNotShowContactUs) {
+        contactUs =<></>
+    }
     return (
         <div className="rounded-2xl p-4 md:p-8 lg:p-12 grid gap-4 md:gap-8 lg:gap-12 bg-purple-900/30 reveal">
             <h3 className="font-thin">{headline}</h3>
             <p>{copy}</p>
-            <div className="font-bold text-cta-500 self-end hover:text-cta-700 mr-auto hover:underline">
-                <a className="content-end text-lg md:text-xl lg:text-2xl" href="#contact-us">Contact Us</a>
-            </div>
+            {contactUs}
         </div>
     );
 }
--- src/Feature.js
+++ src/Feature.js
@@ -6,12 +6,12 @@ function Feature({classes}) {
             <div className={`${classes}`}>
                 <h2 className="text-center">Sell More! Sell Faster!</h2>
                 <div className="grid gap-8 lg:grid-cols-2 grid-cols-1 pb-10">
-                    <Card headline="Unlock Your Business’ True Potential"/>
-                    <Card headline="Tech We Use"/>
+                    <Card headline="Unlock Your Business’ True Potential" doNotShowContactUs="true"/>
+                    <Card headline="Tech We Use" doNotShowContactUs="true"/>
                 </div>
                 <div className="text-center">
                     <a href="#contact-us"
-                       className="no-underline bg-cta-500 rounded-full py-2 md:py-3 px-4 md:px-5 font-bold inline-block hover:bg-cta-700">
+                       className="w-full sm:w-auto no-underline bg-cta-500 rounded-full py-2 md:py-3 px-4 md:px-5 font-bold inline-block hover:bg-cta-700">
                         Contact Us</a>
                 </div>
             </div>
