7623b1464bf265db11b94fd59122b77e4599ae85
Solomon <solomon@devsdecide.com>
2022-02-10 15:58:45 -0600

    update formatting

--- src/ContactUCTA.js
+++ src/ContactUCTA.js
@@ -5,8 +5,8 @@ function ContactUCTA({classes}) {
                className="ctabtn
                         text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl 4xl:text-4xl
                         mt-0 xl:mt-10 2xl:mt-20
-                        py-2 md:py-3 lg:px-4 xl:px-5 2xl:px-6 4xl:px-8
-                        px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 4xl:px-16
+                        py-2 sm:py-3 md:py-4 lg:px-5 xl:px-6 2xl:px-7 4xl:px-9
+                        px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 2xl:px-16 4xl:px-20
                         no-underline bg-cta-700 rounded-full font-bold inline-block">
                 Contact Us</a>
         </div>
--- src/Hero.js
+++ src/Hero.js
@@ -7,12 +7,14 @@ function Hero({classes}) {
                 <div className="object-cover w-full h-full zoomeffect" id="backgroundhero"/>
             </div>
             <div className="absolute top-1/3 left-1/2 -translate-x-1/2 w-full -translate-y-1/4 mx-auto px-10 md:px-14 lg:px-18 xl:px-22 2xl:px-40 4xl:px-34 grid gap-12">
-                <div className={`grid items-center gap-6 lg:grid-cols-2`}>
+                <div className={`grid items-center gap-24 lg:grid-cols-2`}>
                     <div className="grid content-center items-center gap-4 2xl:gap-16">
                         <h1 className="text-center font-bold w-5/6 m-auto">Boost Your Print Shop’s Profits</h1>
                         <sub className="text-center">With just a few custom reports and integrations you can get existing customers coming back for more and new customers buying more.</sub>
                     </div>
-                    <img src={`drawing.svg`} className={`h-3/4 w-3/4 m-auto`} alt={``}/>
+                    <div className={``}>
+                        <img src={`drawing.svg`} className={`h-1/2 w-1/2 lg:h-full lg:w-full mx-auto`} alt={``}/>
+                    </div>
                 </div>
                 <div className={`grid gap-6`}>
                     <ContactUCTA />
