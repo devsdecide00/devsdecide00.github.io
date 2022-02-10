12ac2985063a59c2af9b1354c69a12cb4dbeb501
Solomon <solomon@devsdecide.com>
2022-02-10 16:23:21 -0600

    update formatting

--- src/Hero.js
+++ src/Hero.js
@@ -8,9 +8,9 @@ function Hero({classes}) {
             </div>
             <div className="absolute top-1/3 left-1/2 -translate-x-1/2 w-full -translate-y-1/4 mx-auto px-10 md:px-14 lg:px-18 xl:px-22 2xl:px-40 4xl:px-34 grid gap-12">
                 <div className={`grid items-center gap-24 lg:grid-cols-2`}>
-                    <div className="grid content-center items-center gap-4 2xl:gap-16">
-                        <h1 className="text-center font-bold w-5/6 m-auto">Boost Your Print Shop’s Profits</h1>
-                        <sub className="text-center">With just a few custom reports and integrations you can get existing customers coming back for more and new customers buying more.</sub>
+                    <div className="grid content-center items-center gap-4 2xl:gap-16 h-1/2">
+                        <h1 className="text-center font-bold lg:text-left">Boost Your Print Shop’s Profits</h1>
+                        <sub className="text-center lg:text-left">With just a few custom reports and integrations you can get existing customers coming back for more and new customers buying more.</sub>
                     </div>
                     <div className={``}>
                         <img src={`drawing.svg`} className={`h-1/2 w-1/2 lg:h-full lg:w-full mx-auto`} alt={``}/>
