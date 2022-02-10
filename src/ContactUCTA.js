e9dff075c7b09dc89f91386eefdd84752defbc98
Solomon <solomon@devsdecide.com>
2022-02-09 19:19:08 -0600

    more styling

--- src/ContactUCTA.js
+++ src/ContactUCTA.js
@@ -3,6 +3,7 @@ function ContactUCTA({classes}) {
         <div className={`text-center ${classes}`}>
             <a href="#contact-us"
                className="text-base md:text-base lg:text-lg xl:text-xl 2xl:text-2xl 4xl:text-4xl
+                        mt-0 xl:mt-10 2xl:mt-20
                         py-2 md:py-3 lg:px-4 xl:px-5 2xl:px-6 4xl:px-8
                         px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 4xl:px-16
                         no-underline bg-cta-700 rounded-full font-bold inline-block hover:bg-cta-800">
--- src/Hero.js
+++ src/Hero.js
@@ -6,12 +6,14 @@ function Hero({classes}) {
             <div className="w-full h-full overflow-hidden">
                 <div className="object-cover w-full h-full zoomeffect" id="backgroundhero"/>
             </div>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 w-full -translate-y-1/2 mx-auto px-10 md:px-14 lg:px-18 xl:px-22 2xl:px-26 4xl:px-34">
-                <div className="grid content-center items-center gap-16">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 w-full -translate-y-1/3 mx-auto px-10 md:px-14 lg:px-18 xl:px-22 2xl:px-26 4xl:px-34">
+                <div className="grid content-center items-center gap-4 2xl:gap-16">
                     <h1 className="text-center font-bold w-5/6 m-auto">Boost Your Print Business’ Profits</h1>
                     <sub className="text-center">With custom reports and integrations you can have the full insights into your business you’ve always wanted.</sub>
                     <ContactUCTA />
-                    <div className="indicator m-auto self-end h-16 w-8" />
+                    <div className="indicator m-auto self-end
+                    h-10 md:h-12 lg:h-14 xl:h-16 2xl:h-18
+                    w-5  md:w-6 lg:w-7 xl:w-8 2xl:w-9" />
                 </div>
             </div>
         </div>
