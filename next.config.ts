a86a58fd3f65d57d46429ebc7e16b0ec9252f7a8
slasluisa <solomon@devsdecide.com>
2026-04-21 20:30:04 -0500

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverMaxBodySize: '16kb',
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
